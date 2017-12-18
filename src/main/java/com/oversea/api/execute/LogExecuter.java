78ad17e7af85ab91b42acd986970575cb08e3724
zoubf <zoubf@taofen8.com>
2017-12-18 18:28:50 +0800

    remove interface LogExecuter

--- src/main/java/com/oversea/api/execute/LogExecuter.java
+++
@@ -1,9 +0,0 @@
-package com.oversea.api.execute;
-
-import com.oversea.common.request.RequestBaseParams;
-import com.oversea.common.response.ResponseBaseParams;
-
-public interface LogExecuter {
-	
-	public void log(String from, RequestBaseParams requestParams, ResponseBaseParams responseParams);
-}
