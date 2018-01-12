95f9973c467709bd98fb5f8e4809dd6ca0e579bf
zoubf <zoubf@taofen8.com>
2018-01-12 15:41:52 +0800

    remove useless code

--- src/test/java/com/oversea/api/service/homeService/StartUpTest.java
+++ src/test/java/com/oversea/api/service/homeService/StartUpTest.java
@@ -4,15 +4,12 @@ import javax.annotation.Resource;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.oversea.api.annotation.DBResource;
 import com.oversea.api.util.WorkBookUtils;
-import com.oversea.common.request.ht.ginza.StartupRequest;
-import com.oversea.common.response.ht.ginza.StartupResponse;
 import com.oversea.common.service.HomeService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -29,17 +26,8 @@ public class StartUpTest {
         WorkBookUtils.initDBResource(StartUpTest.class);
     }
     
-
     @AfterClass
     public static void tearDown() throws Exception {
         WorkBookUtils.clearDBResource(StartUpTest.class);
     }
-	
-    
-    @Test
-    public void test() throws Exception{
-    	StartupRequest request = new StartupRequest();
-    	StartupResponse reponse = homeService.startup(request);
-    }
-
 }
